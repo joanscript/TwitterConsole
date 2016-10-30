2e947487aff1abc28bd73fec635e2df7ed2c6c51
oriol <oriolsr@gmail.com>
2016-10-30 19:16:10 +0100

    create wall Printer class

---
+++ production/source/ct/osaludes/messages/ui/WallPrinter.java
@@ -0,0 +1,36 @@
+package ct.osaludes.messages.ui;
+
+import ct.osaludes.messages.model.TimelineMessage;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WallPrinter {
+    private Console console;
+    private FormatDateTime formatDateTime;
+
+    public WallPrinter(Console console, FormatDateTime formatDateTime) {
+        this.console = console;
+        this.formatDateTime = formatDateTime;
+    }
+
+    public void print(List<TimelineMessage> timelineMessages) {
+        Iterator<String> iterator = timelineMessages.stream()
+                .map(timelineMessage -> getDateTimeFormat(timelineMessage, formatDateTime))
+                .collect(Collectors.toCollection(LinkedList::new))
+                .descendingIterator();
+        while(iterator.hasNext()) {
+            console.print(iterator.next());
+        }
+    }
+
+    private String getDateTimeFormat(TimelineMessage timelineMessage, FormatDateTime formatDateTime) {
+        return timelineMessage.getAlias()
+                + " - "
+                + timelineMessage.getMessage()
+                + " "
+                + formatDateTime.format(timelineMessage.getDate());
+    }
+}
