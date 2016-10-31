573bf54c324abc890bc7accb9a9c40d3408b75f6
oriol <oriolsr@gmail.com>
2016-10-31 12:40:49 +0100

    extract minutes difference calculation from formatting text

--- production/source/ct/osaludes/messages/ui/FormatDateTime.java
+++ production/source/ct/osaludes/messages/ui/FormatDateTime.java
@@ -1,6 +1,5 @@
 package ct.osaludes.messages.ui;
 
-import com.sun.javafx.css.converters.EffectConverter;
 import ct.osaludes.messages.infrastructure.Clock;
 
 import java.text.ParseException;
@@ -12,27 +11,14 @@ public class FormatDateTime {
     private Clock clock;
 
     public FormatDateTime(Clock clock) {
-
         this.clock = clock;
     }
 
     public String format(String stringDate) {
         long now = getMillis(clock.getDateTime());
         long millis = getMillis(stringDate);
-        long diffInMillis = millis - now;
-
-        return getFormattedText(diffInMillis);
-    }
-
-    private String getFormattedText(long diffInMillis) {
-        long minutesDifference = TimeUnit.MINUTES.convert(diffInMillis, TimeUnit.MILLISECONDS);
-        if (minutesDifference == 1) {
-            return "(1 minute ago)";
-        } else if (minutesDifference == 0) {
-            return "(less than 1 minute ago)";
-        } else {
-            return "(" + minutesDifference + " minutes ago)";
-        }
+        long minutesDifference = getMinutesDifference(now, millis);
+        return getFormattedText(minutesDifference);
     }
 
     private long getMillis(String stringDate) {
@@ -42,6 +28,21 @@ public class FormatDateTime {
             return date.getTime();
         } catch (ParseException e) {
             return 0;
+        }
+    }
+
+    private long getMinutesDifference(long now, long millis) {
+        long diffInMillis = millis - now;
+        return TimeUnit.MINUTES.convert(diffInMillis, TimeUnit.MILLISECONDS);
+    }
+
+    private String getFormattedText(long minutesDifference) {
+        if (minutesDifference == 1) {
+            return "(1 minute ago)";
+        } else if (minutesDifference == 0) {
+            return "(less than 1 minute ago)";
+        } else {
+            return "(" + minutesDifference + " minutes ago)";
         }
     }
 }
